64089262cff088bc51012987e065cf932077f3bd
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2025-04-28 17:03:55 +0200

    fix

--- app/root.tsx
+++ app/root.tsx
@@ -14,7 +14,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body className={bodyClassName}>
+      <body>
         {children}
         <ScrollRestoration />
         <Scripts />
