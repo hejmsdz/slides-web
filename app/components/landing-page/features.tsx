2111012a84d725099ae4188564f59f98c18c9ffc
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2026-03-08 15:15:51 +0100

    copy update

--- app/components/landing-page/features.tsx
+++ app/components/landing-page/features.tsx
@@ -52,7 +52,7 @@ const Features = () => {
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="text-center mb-16">
           <h2 className="text-4xl md:text-5xl font-bold text-foreground mb-4">
-            Wszystkie potrzebne funkcje
+            Funkcje stworzone z&nbsp;myślą o&nbsp;liturgii
           </h2>
           <p className="text-xl text-muted-foreground max-w-3xl mx-auto">
             Psallite to kompleksowe rozwiązanie do prezentacji pieśni,
